65927242436bbbcb6b7c1c942b5a1f13ab2dddf1
patotskay <94907779+patotskay@users.noreply.github.com>
2022-12-26 16:57:45 +0300

    Первая попытка теста + правка в документацию.

---
+++ main_test.py
@@ -0,0 +1,14 @@
+from fastapi import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_post():
+    
+    article = client.post("/http://51.250.4.66:5000/send_text")
+    assert article == str
+
+def test_get():
+    title = client.get("http://51.250.4.66:5000/get_result")
+    assert title == str
