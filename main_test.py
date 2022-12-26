a304eec8b389848a874f5663f09f04d42fa32271
patotskay <94907779+patotskay@users.noreply.github.com>
2022-12-26 20:57:41 +0300

    попытка теста нам2

--- main_test.py
+++ main_test.py
@@ -1,14 +1,18 @@
-from fastapi import TestClient
-from main import app
+#from fastapi import TestClient
+#from main import app
 
-client = TestClient(app)
+#client = TestClient(app)
 
 
-def test_post():
+#def test_post():
     
-    article = client.post("/http://51.250.4.66:5000/send_text")
-    assert article == str
+    #article = client.post("/http://51.250.4.66:5000/send_text")
+    #assert article == str
 
-def test_get():
-    title = client.get("http://51.250.4.66:5000/get_result")
-    assert title == str
+#def test_get():
+    #title = client.get("http://51.250.4.66:5000/get_result")
+    #assert title == str
+x = input()
+
+def light_test(x):
+    assert x == str
